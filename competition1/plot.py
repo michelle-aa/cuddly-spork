be78b413df88bb509338010c118573ccc8bbb5bc
Michelle Anthony <michelle.a.anthony@gmail.com>
2017-02-05 23:10:34 -0800

    moved plotting code to its own file?

---
+++ competition1/plot.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+def plot_accs(train_accs, test_accs, indices, label):
+    # make teh graph
+    plt.figure()
+    colors = ['crimson', 'darkcyan']
+    plt.plot(indices, train_accs, marker='o', color='crimson', label='train')
+    plt.plot(indices, test_accs, marker='o', color='darkcyan', label='test')
+    plt.xlabel(label)
+    plt.ylabel('average accuracy')
+    plt.legend(loc='best')
+
+    plt.show()
