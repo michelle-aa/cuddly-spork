a3784eedd0809714462acfc9e168a7129c1af042
Michelle Anthony <michelle.a.anthony@gmail.com>
2017-02-05 23:31:20 -0800

    added test code for ensemble forest depths

--- competition1/tree_forest.py
+++ competition1/tree_forest.py
@@ -1,11 +1,12 @@
 import numpy as np
 from sklearn.model_selection import KFold
 from sklearn import ensemble
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 import voterdata_io as vdio
+import plot
 
-TESTING = False
+TESTING = True
 PRINT_LEVEL = 2
 
 if (PRINT_LEVEL >= 1):
@@ -27,25 +28,49 @@ if (PRINT_LEVEL >= 2):
 if (PRINT_LEVEL >= 1):
     print "done loading"
 
-# Actually use the training data to predict the test data
-t_real = ensemble.RandomForestClassifier(n_estimators=101,
-                                            criterion='gini',
-                                              random_state=None,
-                                              max_depth=8,
-                                              n_jobs=-1)
-t_real.fit(x, y)
-print "Test score:", t_real.score(x, y)
-pred_08 = t_real.predict(x_2008)
-print "Predictions 2008:", pred_08[:10]
-pred_12 = t_real.predict(x_2012)
-print "Predictions 2012:", pred_12[:10]
+if TESTING:
+    # estimating test error with cross validation over different max depths
+    NUM_FOLDS = 10
+    kf = KFold(n_splits=NUM_FOLDS)
+    depths = range(2,21)
 
-vdio.write_predictions('2008_test4.csv', pred_08)
-vdio.write_predictions('../competition2/2012_test4.csv', pred_12)
+    train_acc = []
+    test_acc = []
 
+    for d in depths:
+        avg_test_acc = 0.0
+        avg_train_acc = 0.0
 
-try:
-    #plt.show()
-    pass
-except:
-    pass
+        for train_index, test_index in kf.split(x):
+
+            t = ensemble.RandomForestClassifier(n_estimators=101,
+                                                        criterion='gini',
+                                                          random_state=None,
+                                                          max_depth=d,
+                                                          n_jobs=-1)
+            t.fit(x[train_index], y[train_index])
+            avg_train_acc += 1.0 / NUM_FOLDS * t.score(x[train_index], y[train_index])
+            avg_test_acc += 1.0 / NUM_FOLDS * t.score(x[test_index], y[test_index])
+
+        train_acc.append(avg_train_acc)
+        test_acc.append(avg_test_acc)
+    
+    plot.plot_accs(train_acc, test_acc, depths, 'maximum tree depth')
+
+else: # not testing
+    # Actually use the training data to predict the test data
+    t_real = ensemble.RandomForestClassifier(n_estimators=101,
+                                                criterion='gini',
+                                                  random_state=None,
+                                                  max_depth=8,
+                                                  n_jobs=-1)
+    t_real.fit(x, y)
+    print "Test score:", t_real.score(x, y)
+    pred_08 = t_real.predict(x_2008)
+    print "Predictions 2008:", pred_08[:10]
+    pred_12 = t_real.predict(x_2012)
+    print "Predictions 2012:", pred_12[:10]
+
+    vdio.write_predictions('2008_test4.csv', pred_08)
+    vdio.write_predictions('../competition2/2012_test4.csv', pred_12)
+
