c1d6a4a60ec258e089aee357a7d8de1eec5ce9b9
brian112358 <brian112358@gmail.com>
2017-02-07 21:49:27 -0800

    Added initial neural network classification model.

---
+++ competition1/neural_network.py
@@ -0,0 +1,50 @@
+import numpy as np
+import tensorflow as tf 
+import keras
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from sklearn.model_selection import KFold
+
+import voterdata_io as vdio
+import plot
+
+x, y = vdio.get_2008_train()
+x, enc = vdio.process_data(x)
+
+# {1, 2} -> {0, 1}
+y -= 1
+
+model = Sequential()
+model.add(Dropout(0.2, input_shape=np.shape(x[0])))
+model.add(Dense(80))
+model.add(Activation('relu'))
+# model.add(Dropout(0.2))
+model.add(Dense(40))
+model.add(Activation('relu'))
+model.add(Dense(20))
+model.add(Activation('relu'))
+# model.add(Dropout(0.2))
+model.add(Dense(5))
+model.add(Activation('relu'))
+
+# Output layer
+model.add(Dense(1, init='normal', activation='sigmoid'))
+
+model.summary()
+
+model.compile(loss='binary_crossentropy', optimizer='adam',
+    metrics=['accuracy'])
+
+# estimating test error with cross validation over different max depths
+NUM_FOLDS = 2
+kf = KFold(n_splits=NUM_FOLDS)
+
+avg_train_acc = 0
+avg_test_acc = 0
+for train_index, test_index in kf.split(x):
+    model.fit(x[train_index], y[train_index], batch_size=32, nb_epoch=50, verbose=1)
+    avg_train_acc += 1.0 / NUM_FOLDS * model.evaluate(x[train_index], y[train_index])[1]
+    avg_test_acc += 1.0 / NUM_FOLDS * model.evaluate(x[test_index], y[test_index])[1]
+
+print 'Train accuracy:', avg_train_acc
+print 'Test accuracy:', avg_test_acc
