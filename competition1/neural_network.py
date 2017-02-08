22e11480c8923ec9a62f2086a62b8df13c6654ff
brian112358 <brian112358@gmail.com>
2017-02-07 22:38:47 -0800

    Used validation_split from Keras instead of sklearn's KFold validation.

--- competition1/neural_network.py
+++ competition1/neural_network.py
@@ -43,22 +43,11 @@ def create_model():
         metrics=['accuracy'])
     return model
 
-# estimating test error with cross validation over different max depths
-NUM_FOLDS = 4
-kf = KFold(n_splits=NUM_FOLDS)
 
-avg_train_acc = 0
-avg_test_acc = 0
-for train_index, test_index in kf.split(x):
-    model = create_model()
-    model.fit(x[train_index], y[train_index], batch_size=32, nb_epoch=50, verbose=1)
-    avg_train_acc += 1.0 / NUM_FOLDS * model.evaluate(x[train_index], y[train_index])[1]
-    avg_test_acc += 1.0 / NUM_FOLDS * model.evaluate(x[test_index], y[test_index])[1]
+model = create_model()
+model.fit(x, y, batch_size=256, nb_epoch=50, validation_split=0.2, verbose=1)
+score = model.evaluate(x, y)
 
-print 'Train accuracy:', avg_train_acc
-print 'Test accuracy:', avg_test_acc
-
-
-
-
-
+print ''
+print 'Training Loss:', score[0]
+print 'Accuracy:', score[1]
