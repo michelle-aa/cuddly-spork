abdde8644e8a86be50b063c2cffcf0c3f2fae07a
carlyrobison <carly.ellen.robison@gmail.com>
2017-02-09 02:39:47 -0800

    Tried a little harder on the neural net but it gives me floats between 0 and 1

--- competition1/neural_network.py
+++ competition1/neural_network.py
@@ -48,6 +48,20 @@ model = create_model()
 model.fit(x, y, batch_size=256, nb_epoch=50, validation_split=0.2, verbose=1)
 score = model.evaluate(x, y)
 
+# Predictions!
+x_2008 = vdio.get_2008_test()
+x_2008, enc = vdio.process_data(x_2008)
+x_2012 = vdio.get_2012_test()
+x_2012, enc = vdio.process_data(x_2012)
+
+pred_08 = model.predict(x_2008)
+print "Predictions 2008:", pred_08[:10]
+pred_12 = model.predict(x_2012)
+print "Predictions 2012:", pred_12[:10]
+
+vdio.write_predictions('2008_test_nn.csv', pred_08)
+vdio.write_predictions('../competition2/2012_test_nn.csv', pred_12)
+
 print ''
 print 'Training Loss:', score[0]
 print 'Accuracy:', score[1]
